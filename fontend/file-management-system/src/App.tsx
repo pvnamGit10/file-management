d052f702d73d3d99b0581122097d93d5dfbee4f2
pvnamGit10 <pvnam2001@gmail.com>
2022-03-31 22:06:44 +0700

    add frontend

---
+++ fontend/file-management-system/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CommandPromt from './components/CommandPromt.js';
+
+function App() {
+  return (
+    <div>
+      <CommandPromt />
+    </div>
+  );
+}
+
+export default App;
