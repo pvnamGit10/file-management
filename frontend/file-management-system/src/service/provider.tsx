d05ab5723d1f924a97b9b880f23cc3d4e9fc5545
pvnamGit10 <pvnam2001@gmail.com>
2022-04-01 23:59:28 +0700

    create provider api frontend

---
+++ frontend/file-management-system/src/service/provider.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { handleError, handleResponse } from "./response";
+
+const BASE_URL = 'http://localhost:8080/api/v1/terminal';
+
+export const getData = (query: any) => {
+    return axios.get(BASE_URL, {
+        params: {query}
+    })
+        .then(response => handleResponse(response))
+        .catch(error => handleError(error.response))
+}
+
+export const postData = (input: any) => {
+    return axios.post(BASE_URL, input)
+        .then(response => handleResponse(response))
+        .catch(error => handleError(error.response))
+}
+
+export const putData = (input: any) => {
+    return axios.put(BASE_URL, input)
+        .then(response => handleResponse(response))
+        .catch(error => handleError(error.response))
+}
+
+export const deleteData = (query: any) => {
+    return axios.delete(BASE_URL, {
+        params: {query}
+    })
+        .then(response => handleResponse(response))
+        .catch(error => handleError(error.response))
+}
